42bb550c8807d0b4d12045a110deed8f78c878a9
hannahhodor <hannahhodor@gmail.com>
2022-10-12 23:54:58 -0700

    Lab 4
    10/12/22

---
+++ lab4/js/lab.js
@@ -0,0 +1,23 @@
+// Summary. (use period)
+// Description. (use period)
+// @link  URL
+// @file This file defines the MyClass class.
+// @author AuthorName.
+// @since x.x.x
+// Print the board before each turn
+  // Loop through each row
+    // Loop through each space
+      // Print X, O, or Space
+      // Print a | between
+    // Print a newline at the end
+    // Print a horizontal line between
+
+// How to play a puzzle online.
+  // Pull up an online puzzle.
+    // Find corner pieces.
+      // Match pieces as best you can until the border is made.
+      // Match pieces of similar color.
+        // Start pairing everything together to see if it fits together.
+        // Make bigger pieces.
+      // Keep going until you finish the puzzle.
+  // Enjoy and wonder why Wolverine's power is healthcare or how we've never seen a gorilla at full strength since they don't lift.
