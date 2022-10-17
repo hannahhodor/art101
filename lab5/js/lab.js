77b1fc338ab21b7019b11b7c8be6f493d3acc146
hannahhodor <hannahhodor@gmail.com>
2022-10-17 16:16:08 -0700

    Lab5
    JavaScript Data types and variables

---
+++ lab5/js/lab.js
@@ -0,0 +1,23 @@
+/*
+ * Author: Emily Baer <egbaer@ucsc.edu> and Hannah Hodor <hhodor@ucsc.edu>
+ * Created 17 October
+ * License: Public Domain
+ */
+
+ // Define Variables
+ make="Poorsche";
+ model="BML";
+ color="chartreuse";
+ year=2030;
+ ownIt=true;
+
+ // calculate
+ age=2022-year;
+
+
+ // output
+  document.writeln("Make: " + make + "<br>");
+  document.writeln("Model: " + model + "<br>");
+  document.writeln("Color: " + color + "<br>");
+  document.writeln("Year: " + year + "<br>");
+  document.writeln("Age: " + age + "years<br>");
